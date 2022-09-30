e9fcb2bd4b1b584d2d7573ca74fce3f7022dd62b
Juan Martín Núñez Aréchiga <jmartin.nuar@gmail.com>
2022-09-30 00:59:30 -0500

    Ejercicio resuelto

--- src/MangosNaranjas.java
+++ src/MangosNaranjas.java
@@ -1,4 +1,42 @@
 
 public class MangosNaranjas {
 	
-}
+	private int mangos;
+	private int naranjas;
+	public MangosNaranjas(int mangos, int naranjas) {
+		this.mangos = mangos;
+		this.naranjas = naranjas;
+	} // MangosNaranjas
+	
+	public void reparto(int mangos, int naranjas) {
+		int totalPorCaja = (mangos + naranjas) / (maxCajas(mangos, naranjas));
+		
+		double mangosPorCaja = ((double) mangos/((double) naranjas + (double) mangos)) * totalPorCaja;
+		double naranjasPorCaja = ((double)naranjas/((double)naranjas + (double)mangos)) * totalPorCaja;
+		
+		System.out.println("Los mangos por caja son: " + String.format("%.0f", mangosPorCaja));
+		System.out.println("Las naranjas por caja son: " + String.format("%.0f", naranjasPorCaja));
+	} // reparto
+	
+	public int maxCajas(int mangos, int naranjas) {
+			 
+	        int max = Math.max(mangos, naranjas); // El número más grande de los ingresados
+	        int min = Math.min(mangos, naranjas); // El número más pequeño de los ingresados
+	 
+	        int cajas = 0;
+	        do {
+	            cajas = min;
+	            min = max % min;
+	            max = cajas;
+	 
+	        } while (min != 0);
+	        
+	        return cajas;
+	        
+	    } // maxCajas
+	
+	public void resultadoMaxCajas(int mangos, int naranjas) {
+		System.out.println("El número máximo de cajas es: " + maxCajas(mangos, naranjas));
+	}
+	
+} // MangosNaranjas
--- src/ResultadoCajas.java
+++ src/ResultadoCajas.java
@@ -3,10 +3,30 @@ import java.util.Scanner;
 public class ResultadoCajas {
 
 	public static void main(String[] args) {
+		
+		// Se abre el scanner para ingresar los datos con el teclado
 		Scanner sc = new Scanner(System.in);
-	     int mangos = sc.nextInt();
-	     int naranjas = sc.nextInt();
-	     MangosNaranjas(mangos, naranjas);
+		
+		// En la consola manda mensaje y pide ingresar las cantidades
+		// de mangos y naranjas
+		
+		System.out.println("Ingresa el número de mangos: ");
+	    int mangos = sc.nextInt();
+	     
+	    System.out.println("Ingresa el número de naranjas");
+	    int naranjas = sc.nextInt();
+	    
+	    // Crea una nueva clase con los datos ingresados
+
+	    MangosNaranjas mangosNaranjas = new MangosNaranjas(mangos, naranjas);
+	    
+	    // Imprime el número máximo de cajas
+		mangosNaranjas.resultadoMaxCajas(mangos, naranjas);
+		
+		// Imprime la cantidad de mangos y naranjas por caja
+		
+		mangosNaranjas.reparto(mangos, naranjas);
+		
 	}
 
-}
+} // ResultadoCajas
