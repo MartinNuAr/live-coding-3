58c6e39e0181ed24e0445ab03d2e7ba53e5351b1
Juan Martín Núñez Aréchiga <jmartin.nuar@gmail.com>
2022-09-29 23:19:37 -0500

    chore | Se inicializa el repositorio

---
+++ src/ResultadoCajas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ResultadoCajas {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	     int mangos = sc.nextInt();
+	     int naranjas = sc.nextInt();
+	     MangosNaranjas(mangos, naranjas);
+	}
+
+}
